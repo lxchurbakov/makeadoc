7f9283e94bb62cc6147c60500088580732b0f51c
lxchurbakov <lxchurbakov@gmail.com>
2022-12-15 09:08:08 +0100

    Integrate templates api

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 
-// const unzip = require('unzip');
-const decompress = require('decompress');
-const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { templates } = require('./routes/templates');
 
 const app = express();
 
@@ -25,53 +22,7 @@ app.use(express.json());
 
 // Routes go here
 
-app.post('/templates', upload.single('file'), (req, res) => {
-    const templateZip = req.file;
-    
-    // if (!templateZip)
-
-    // Rename file to add zip extension
-    fs.rename(templateZip.path, templateZip.path + '.zip', (err) => {
-        // if err
-        decompress(templateZip.path + '.zip', "templates")
-            .then((files) => {
-                console.log(files);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-        // const name = templateZip.originalname.split('.')[0];
-// 
-        // fs.createReadStream(templateZip.path).pipe(unzip.Extract({ path: `templates/${name}` }));
-        console.log(err)
-    });
-
-
-    // console.log(templateZip)
-
-    // fieldname: 'file',
-    // originalname: 'example-template.zip',
-    // encoding: '7bit',
-    // mimetype: 'application/zip',
-    // destination: 'uploads/',
-    // filename: 'd0baa1b02e90d2fd8684ac0c56bc9335',
-    // path: 'uploads/d0baa1b02e90d2fd8684ac0c56bc9335',
-    // size: 184
-
-    
-    
-
-    
-
-
-    // console.log(req);
-    res.json('ok');
-});
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.use('/templates', templates);
 
 // Error handling
 
