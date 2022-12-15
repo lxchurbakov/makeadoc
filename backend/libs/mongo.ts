6b38b74d057f661d1a6ec13858592e9c64398ace
lxchurbakov <lxchurbakov@gmail.com>
2022-12-15 09:07:21 +0100

    Create mongo helper on be

---
+++ backend/libs/mongo.ts
@@ -0,0 +1,14 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+const URI = 'mongodb://root:example@localhost:27017/?maxPoolSize=20&w=majority';
+
+const client = new MongoClient(URI);
+const database = client.db('makeadoc');
+
+export { ObjectId };
+export const Template = database.collection('templates');
+
+client.connect().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
